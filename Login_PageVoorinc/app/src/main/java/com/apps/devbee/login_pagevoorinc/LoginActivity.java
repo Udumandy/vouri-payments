7020f8201f99a4836e7ea044587bcc2a7f604320
Udumandy <udumandy@yahoo.com>
2019-07-03 20:49:59 +0100

    AsyncTask and homework

--- Login_PageVoorinc/app/src/main/java/com/apps/devbee/login_pagevoorinc/LoginActivity.java
+++ Login_PageVoorinc/app/src/main/java/com/apps/devbee/login_pagevoorinc/LoginActivity.java
@@ -1,9 +1,16 @@
 package com.apps.devbee.login_pagevoorinc;
 
+import android.content.Context;
 import android.graphics.drawable.AnimationDrawable;
+import android.os.AsyncTask;
 import android.os.PersistableBundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.AsyncTaskLoader;
+import android.support.v4.content.Loader;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -15,7 +22,10 @@ import android.widget.Toast;
 import com.apps.devbee.login_pagevoorinc.loginTwoFragment.LoginFragment;
 import com.apps.devbee.login_pagevoorinc.loginTwoFragment.Sign_upFragment;
 
-public class LoginActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class LoginActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<List<Integer>> {
     ViewPager login_two_viewpager;
     TabLayout tabLayout;
     ImageView logo_vouriinc;
@@ -60,6 +70,40 @@ public class LoginActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        new AsyncTask<Integer, Void, Integer>() {
+
+            @Override
+            protected Integer doInBackground(Integer... integers) {
+                int sums = 0;
+                for (i = 0; i < 2000000; i++) {
+                    sums += i;
+                }
+                    return sums;
+                }
+
+                @Override
+                protected void onPostExecute (Integer integer){
+                    super.onPostExecute(integer);
+                    Toast.makeText(getApplicationContext(), "" +sum , Toast.LENGTH_SHORT).show();
+                }
+
+                @Override
+                protected void onPreExecute () {
+                    super.onPreExecute();
+                }
+
+                @Override
+                protected void onProgressUpdate (Void...values){
+                    super.onProgressUpdate(values);
+                }
+            }//.execute()
+         ;
+
+        getSupportLoaderManager().initLoader(0,new Bundle(), this);
+        }
     public ImageView getLogo_vouriinc() {
         return logo_vouriinc;
     }
@@ -68,5 +112,38 @@ public class LoginActivity extends AppCompatActivity {
         return progressBar;
     }
 
+    @NonNull
+    @Override
+    public Loader<List<Integer>> onCreateLoader(int i, @Nullable Bundle bundle) {
+        return new StringListLoader(this);
+    }
+
+    @Override
+    public void onLoadFinished(@NonNull Loader<List<Integer>> loader, List<Integer> data) {
+        Toast.makeText(this, "Operation Complete", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader loader) {
+
+    }
+
+    public static class StringListLoader extends AsyncTaskLoader<List<Integer>> {
+
+
+        public StringListLoader(@NonNull Context context) {
+            super(context);
+        }
+
+        @Nullable
+        @Override
+        public List<Integer> loadInBackground() {
+            List<Integer> list = new ArrayList<>(2000000);
+            for(int y =0; y < 2000000; y++){
+                list.add(y);
+            }
+            return list;
+        }
+    }
 
 }
